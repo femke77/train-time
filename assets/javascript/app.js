2d79e12f8f48169014ecabe26d01d007ed69400b
Meg Meyers <megan.g.meyers@gmail.com>
2019-11-30 14:51:52 -0800

    css set up done

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDvjZfMlhV3jzu05gOeSFxY95vPnggD58s",
+    authDomain: "train-time-c3b1a.firebaseapp.com",
+    databaseURL: "https://train-time-c3b1a.firebaseio.com",
+    projectId: "train-time-c3b1a",
+    storageBucket: "train-time-c3b1a.appspot.com",
+    messagingSenderId: "40276663912",
+    appId: "1:40276663912:web:6e04cf6046bd53a79b65dd"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
