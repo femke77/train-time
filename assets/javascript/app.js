6687eff79b3b2359e9e7bb607ac45448c175a934
Meg Meyers <megan.g.meyers@gmail.com>
2019-11-30 15:00:43 -0800

    add train button adding train obj to database

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,4 +12,38 @@ var firebaseConfig = {
 
   var database = firebase.database();
 
+  $("#add-train-btn").on("click", function(event){
+    event.preventDefault();
+
+    // Grabs user input
+    var empName = $("#train-name-input").val().trim();
+    var empRole = $("#dest-input").val().trim();
+    var empStart = moment($("#first-time-input").val().trim(), "HH:mm").format("X");
+    var empRate = $("#freq-input").val().trim();
   
+    // Creates local "temporary" object for holding train data
+    var train = {
+      name: trainName,
+      dest: trainDest,
+      firstStartTime: firstStartTime,
+      freq: trainFreq
+    };
+  
+    // Uploads employee data to the database
+    database.ref().push(train);
+  
+    // Logs everything to console
+    console.log(train.name);
+    console.log(train.trainDest);
+    console.log(train.firstStartTime);
+    console.log(train.trainFreq);
+  
+    alert("Train successfully added");
+  
+    // Clears all of the text-boxes
+    $("#train-name-input").val("");
+    $("#dest-input").val("");
+    $("#first-time-input").val("");
+    $("#freq-input").val("");
+
+  });
